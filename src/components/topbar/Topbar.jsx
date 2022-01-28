f4bee5f3e2fbad879f9514d23c5da687db54a63d
Loren Cipriano <loren25cipriano@gmail.com>
2022-01-28 11:01:02 -0800

    install material ui package and utilize material ui icons + left container css

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,17 +1,29 @@
 import './topbar.scss'
+import {Person, Mail} from '@material-ui/icons'
 
 export default function Topbar() {
     return (
         <div className='topbar'>
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#intro" className="logo"> LoCodes.</a>
-
+            <div className='wrapper'>
+                <div className='left'>
+                    <a href='#intro' className='logo'> LoCodes </a>
+                    <div className='itemContainer' >
+                        <Person className='icon'/>
+                        <span> +1(702) 885-1800 </span>
+                    </div>
+                    <div className='itemContainer' >
+                        <Mail className='icon' />
+                        <span> loren25cipriano@gmail.com</span>
                     </div>
 
                 </div>
                 <h1>Hello left</h1>
-                <div className="right" >
+                <div className='right' >
+                    <div className='hamburger'>
+                        <span className='line1'></span>
+                        <span className='line2'></span>
+                        <span className='line3'></span>
+                    </div>
                     this is right 
                     
 
