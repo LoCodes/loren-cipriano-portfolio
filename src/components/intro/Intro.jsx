70a7a30a83326ffd07c26fb05a12ce8a88a75685
Loren Cipriano <loren25cipriano@gmail.com>
2022-02-03 08:32:45 -0800

    create left side items in intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,8 +2,25 @@ import './intro.scss'
 
 export default function Intro() {
     return (
-        <div className='intro' id='intro'>
-            intro
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/IMG_3919.png" alt="" />
+                </div>
+            </div>
+
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Loren Cipriano</h1>
+                    <h3>
+            {/* Freelance <span ref={textRef}></span> */}
+          </h3>
         </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
     )
 }
