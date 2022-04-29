8d9f60e791f8428d888d4103c8ad0e67cf9ff08f
Loren Cipriano <loren25cipriano@gmail.com>
2022-04-29 10:44:35 -0700

    update photos for projects in portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -13,38 +13,45 @@ export default function Portfolio() {
             <div className="container">
             
                 <div className="item">
-                    <img src="assets/pet_saver.png"alt="" />       
+                    <h3>Task Tracker</h3>
+                    <img src="assets/task_tracker2.png"alt="" />       
                 </div>
-                <h3>Task Tracker</h3>
+                
                 
 
                 <div className="item">
-                    <img src="assests/art_gallery.png" alt="" />    
-                </div>
                     <h3>Art Gallery Shop</h3>
+                    <img src="assets/art_gallery.png"alt="" />       
+                </div>
+                    
                 
 
                 <div className="item">
-                    <img src="assets/pet_saver.png"alt="" />
-                </div>
                     <h3>Pet Saver</h3>
+                    <img src="assets/pet_saver2.png"alt="" />
+                </div>
+                    
                 
 
 
                 <div className="item">
-                    <img src="assets/toon_web.png"alt="" />
-                </div>
                 <h3>ToonWeb App</h3>
-
-                <div className="item">
-                    <img src="assests/skate_sesh.png" alt="" />       
+                    <img src="assets/toon_web.png"alt="" />
+                    
                 </div>
-                <h3>Skate Sesh </h3>
+                
 
                 <div className="item">
+                    <h3>Skate Sesh </h3>
+                    <img src="assets/skate_sesh.png"alt="" />
+                </div>
+                
+
+                <div className="item">
+                    <h3>Great Houses CLI</h3>
                     <img src="assets/great_houses_cli.png" alt="" />
                 </div>
-                <h3>Great Houses CLI</h3>
+                
 
                 
             </div> 
